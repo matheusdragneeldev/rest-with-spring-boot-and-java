f2e5a044ec7ad3f749a92d1d003561fd77b76bf7
Matheus Dragneel <matheusdragneeldev@gmail.com>
2025-06-14 10:58:20 -0300

    My first steps with Java Spring Boot 3.5.0

---
+++ rest-with-spring-boot-and-java/src/main/java/matheusdragneeldev/com/model/Greeting.java
@@ -0,0 +1,3 @@
+package matheusdragneeldev.com.model;
+
+public record Greeting(long id, String content) {}
---
+++ rest-with-spring-boot-and-java/src/test/java/matheusdragneeldev/com/StartupTests.java
@@ -0,0 +1,13 @@
+package matheusdragneeldev.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
